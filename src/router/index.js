55d84e035d5d8f715f2f2b3f674b9b36eab351be
Huynh Hao Duong <haohuynh230701@gmail.com>
2022-10-28 22:24:45 +0700

    Create an add page

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,12 @@ const routes = [
         component: () => import('@/views/ContactEdit.vue'),
         props: (route) => ({ contactId: parseInt(route.params.id) })
     },
+
+    {
+        path: '/contacts',
+        name: 'contact.add',
+        component: () => import('@/views/ContactAdd.vue')
+    }
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
